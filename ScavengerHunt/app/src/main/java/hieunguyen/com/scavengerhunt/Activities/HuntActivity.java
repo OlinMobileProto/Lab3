e67f46d79d53d94d4e8b87203385e0a98d078fa5
Patrick Huston <patrickhuston1@gmail.com>
2015-10-11 13:49:19 -0400

    Creates HuntActivity

---
+++ ScavengerHunt/app/src/main/java/hieunguyen/com/scavengerhunt/Activities/HuntActivity.java
@@ -0,0 +1,31 @@
+package hieunguyen.com.scavengerhunt.Activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import hieunguyen.com.scavengerhunt.R;
+
+public class HuntActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hunt);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
