1d4ef970314b28baff46474ff1d90b2a4d3274fa
Patrick Huston <patrickhuston1@gmail.com>
2015-10-13 19:56:56 -0400

    Fixes .gitignore messups

--- ScavengerHunt/app/src/main/java/hieunguyen/com/scavengerhunt/Activities/HuntActivity.java
+++ ScavengerHunt/app/src/main/java/hieunguyen/com/scavengerhunt/Activities/HuntActivity.java
@@ -5,9 +5,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 import hieunguyen.com.scavengerhunt.Fragments.ClueFragment;
+import hieunguyen.com.scavengerhunt.Fragments.MapFragment;
 import hieunguyen.com.scavengerhunt.R;
 
-public class HuntActivity extends AppCompatActivity {
+public class HuntActivity extends AppCompatActivity implements ClueFragment.onVideoDoneListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +24,12 @@ public class HuntActivity extends AppCompatActivity {
 
     public Uri getVideoUrl(int clueNumber){
         return Uri.parse("https://s3.amazonaws.com/olin-mobile-proto/MVI_3140.MOV");
+    }
+
+    @Override
+    public void onVideoDone() {
+        getFragmentManager().beginTransaction()
+                .replace(R.id.container, new MapFragment())
+                .commit();
     }
 }
