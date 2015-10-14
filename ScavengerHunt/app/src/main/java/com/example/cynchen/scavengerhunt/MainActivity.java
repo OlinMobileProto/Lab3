fd20ec697ed156f3945b4fe033ca62dccc06c618
Zoher Ghadyali <zoher.ghadyali@gmail.com>
2015-10-13 22:02:14 -0400

    Getting and playing clue and open camera

--- ScavengerHunt/app/src/main/java/com/example/cynchen/scavengerhunt/MainActivity.java
+++ ScavengerHunt/app/src/main/java/com/example/cynchen/scavengerhunt/MainActivity.java
@@ -1,26 +1,32 @@
 package com.example.cynchen.scavengerhunt;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //initializes the searchGoogle fragment and loads it in the container
+        VideoFragment playClue = new VideoFragment();
+        CameraFragment takePicture = new CameraFragment();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        transitionToFragment(takePicture);
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    public void transitionToFragment(Fragment fragment){
+        //This function takes as input a fragment, initializes the fragment manager and replaces
+        //the container with the provided fragment
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.commit();
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
