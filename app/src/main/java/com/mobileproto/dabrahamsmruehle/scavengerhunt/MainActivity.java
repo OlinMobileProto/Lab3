b91852d5fde247723444eecdd38593abdac0fa9b
David Abrahams <david.abrahams@students.olin.edu>
2015-10-15 19:05:10 -0400

    attempted to add navigatable fragments

--- app/src/main/java/com/mobileproto/dabrahamsmruehle/scavengerhunt/MainActivity.java
+++ app/src/main/java/com/mobileproto/dabrahamsmruehle/scavengerhunt/MainActivity.java
@@ -1,36 +1,36 @@
 package com.mobileproto.dabrahamsmruehle.scavengerhunt;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements
-        StartMenuFragment.OnFragmentInteractionListener
+        StartMenuFragment.OnFragmentInteractionListener, HUDFragment.OnFragmentInteractionListener
 {
+
+    FragmentManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener()
-        {
-            @Override
-            public void onClick(View view)
-            {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        manager = getSupportFragmentManager();
+        switchFragment(StartMenuFragment.newInstance());
+    }
+
+    private void switchFragment(Fragment f)
+    {
+
+        Log.d("DEBUG", "boom");
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.container, f);
+        transaction.commit();
     }
 
     @Override
@@ -55,6 +55,15 @@ public class MainActivity extends AppCompatActivity implements
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(char buttonName)
+    {
+        if (buttonName == 'a')
+        {
+            switchFragment(HUDFragment.newInstance());
+        }
     }
 
     @Override
