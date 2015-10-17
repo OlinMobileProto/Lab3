6f2a3031b4d106d9dd0faf464e00801cda4ef05c
Patrick Huston <patrickhuston1@gmail.com>
2015-10-17 11:14:12 -0400

    Creates camera intent'

--- ScavengerHunt/app/src/main/java/hieunguyen/com/scavengerhunt/Fragments/CameraFragment.java
+++ ScavengerHunt/app/src/main/java/hieunguyen/com/scavengerhunt/Fragments/CameraFragment.java
@@ -1,9 +1,11 @@
 package hieunguyen.com.scavengerhunt.Fragments;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.app.Fragment;
+import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,7 +25,11 @@ public class CameraFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_camera, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_camera, container, false);
+
+        dispatchTakePictureIntent();
+
+        return rootView;
     }
 
     public void onButtonPressed(Uri uri) {
@@ -49,19 +55,18 @@ public class CameraFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p/>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         public void onFragmentInteraction(Uri uri);
+    }
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
     }
 
 }
