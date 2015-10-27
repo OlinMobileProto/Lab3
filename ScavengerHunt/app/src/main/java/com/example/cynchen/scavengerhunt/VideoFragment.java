0b3f15ed4512584cf5e4af6fa6f4f2505bddfd0b
yunhsincynthiachen <yun-hsin.chen@students.olin.edu>
2015-10-26 22:48:49 -0400

    with some videofragment changes

--- ScavengerHunt/app/src/main/java/com/example/cynchen/scavengerhunt/VideoFragment.java
+++ ScavengerHunt/app/src/main/java/com/example/cynchen/scavengerhunt/VideoFragment.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
 import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
@@ -32,7 +33,6 @@ public class VideoFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
 
         cluesLink = ((MainActivity)getActivity()).locations_videos;
 
@@ -76,4 +76,5 @@ public class VideoFragment extends Fragment {
         });
         return rootView;
     }
+
 }
